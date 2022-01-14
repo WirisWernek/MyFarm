1b752cdf657d6761b6f6710d0e3d90246c9d7db5
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-14 17:52:50 -0300

    Remoçao de codigo comentado

--- vacinas/actions_vacinar/delete.php
+++ vacinas/actions_vacinar/delete.php
@@ -1,20 +1,7 @@
 <?php
 session_start();
-// require_once '../../includes/db_connect.php';
 require_once '../../includes/class/vacinacao.php';
 if(isset($_POST['btn-deletar'])){
-
-    // $id = mysqli_escape_string($connect, $_POST['id']);
-
-    // $sql = "DELETE FROM tb_vacinados WHERE id_vacinado = '$id'";
-
-    // if(mysqli_query($connect, $sql)){
-    //     $_SESSION['mensagem']= "Excluído com sucesso!";
-    //     header('Location: ../../index.php');
-    // }else{
-    //     $_SESSION['mensagem']= "Erro ao Excluir!";
-    //     header('Location: ../../index.php');
-    // }
     $vacinacao = new Vacinacao($_POST['id'], "", "", "");
     $vacinacao->Excluir();
 }
